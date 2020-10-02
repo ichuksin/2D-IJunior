526e8cab3c9b0706847631b93dd2e263e7c3892d
ichuksin <i.chuksin@gmail.com>
2020-10-02 22:51:18 +0300

    Add MainMenu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] GameObject _panel;
+    public void OnClickExitButton()
+    {
+        Application.Quit();
+    }
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void OnClickAboutButton()
+    {
+        _panel.SetActive(true);
+    }
+    public void OnClickCloseButton()
+    {
+        _panel.SetActive(false);
+    }
+
+}

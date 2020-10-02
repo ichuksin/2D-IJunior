dfabb68a0daad2189a35c6eb8784e6f928d41d1a
ichuksin <i.chuksin@gmail.com>
2020-10-02 20:37:49 +0300

    добавление DOTween

--- Assets/Scripts/Siren.cs
+++ Assets/Scripts/Siren.cs
@@ -1,17 +1,17 @@
 ﻿using UnityEngine;
+using DG.Tweening;
+
 public class Siren : MonoBehaviour
 {
     [SerializeField] private Door _door;
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private float _increaseVolumeInSeconds;
     
-    private float _currentVolume = 0;
-    private bool _reduction = false;
-
     private void OnEnable()
     {
         _door.DoorOpen += OnDoorOpen;
     }
+    
     private void OnDisable()
     {
         _door.DoorOpen -= OnDoorOpen;
@@ -21,43 +21,18 @@ public class Siren : MonoBehaviour
     {
         if (doorOpen)
         {
-            _audioSource.volume = _currentVolume;
-            _reduction = false;
             if (!_audioSource.isPlaying)
                 _audioSource.Play();
+            _audioSource.DOFade(1, _increaseVolumeInSeconds);
         }
         else
         {
-            _reduction = true;
+            _audioSource.DOFade(0, _increaseVolumeInSeconds).onComplete += OnComplete ;            
         }
     }
-
-    private void Update()
+    
+    private void OnComplete()
     {
-        if (_audioSource.isPlaying)
-        {
-            if (!_reduction)
-            {
-                if (_currentVolume < 1.0f)
-                {
-                    _currentVolume += Mathf.Lerp(0, 1, Time.deltaTime / _increaseVolumeInSeconds);
-                    _audioSource.volume = _currentVolume;
-                }
-            }
-            else
-            {
-                if (_currentVolume > 0.0f)
-                {
-                    _currentVolume -= Mathf.Lerp(0, 1, Time.deltaTime / _increaseVolumeInSeconds);
-                    _audioSource.volume = _currentVolume;
-                }
-                else
-                {
-                    _reduction = false;
-                    _currentVolume = 0;
-                    _audioSource.Stop();
-                }
-            }
-        }
+        _audioSource.Stop();
     }
 }
